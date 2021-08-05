457ba4bf5abf0e76be588920d5d16af6889cf8ab
ggornes <gerardogornes@gmail.com>
2021-08-05 19:46:02 +0800

    added missing import | dockerized application

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,16 @@ ENV PATH /app_v1/node_modules/.bin:$PATH
 
 # install app dependencies
 COPY package.json ./
-COPY package-lock.json ./
-RUN npm install --silent
-RUN npm install react-scripts@3.4.1 -g --silent
+# COPY package-lock.json ./
+COPY yarn.lock ./
+COPY src/artifacts ./src
+# RUN npm install --silent
+# RUN npm install react-scripts@3.4.1 -g --silent
+RUN yarn install
 
 # add app
 COPY . ./
 
 # start app
-CMD ["npm", "start"]
+# CMD ["npm", "start"]
+CMD ["yarn", "start"]
--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 
 import Web3ConnectionManager from './components/Web3ConnectionManager';
-import AppBarCustom from './components/AppBarCustom'
-import AppBarElevate from './components/AppBarElevate'
-import SampleContracts from './components/SampleContracts';
-import PolizaContract from './components/PolizaContract';
+// import AppBarCustom from './components/AppBarCustom'
+// import AppBarElevate from './components/AppBarElevate'
+// import SampleContracts from './components/SampleContracts';
+// import PolizaContract from './components/PolizaContract';
 import AppBarDrawer from './components/AppBarDrawer'
-import Fd from './components/Fd'
+// import Fd from './components/Fd'
 
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+// import Container from '@material-ui/core/Container';
+// import Typography from '@material-ui/core/Typography';
+// import Box from '@material-ui/core/Box';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { useReducer, createContext } from 'react'
@@ -54,9 +54,6 @@ function App() {
   return (
 
 
-
-
-
     <div className="App">
       <AppContext.Provider value={{ state, dispatch }}>
         <Web3ConnectionManager/>
@@ -70,37 +67,6 @@ function App() {
       </AppContext.Provider>
     
     </div>    
-
-    // <div className="App">
-    //   <AppBarCustom/>
-    //   <Web3ConnectionManager />
-    //   <Container maxWidth="sm">
-    //     <Box my={4}>
-    //     {/* <Typography variant="h4" component="h1" gutterBottom>
-    //       Hello there
-    //     </Typography> */}
-    //       <Fd/>
-    //     </Box>
-    //   </Container>      
-    // </div>
-
-
-
-
-    
-
-
-
-      // <div className="App">
-      //   <AppBarCustom/>
-      //   {/* <AppBarElevate/> */}
-      //   <Web3ConnectionManager />
-      //   <div className="App-header">
-      //     {/* <SampleContracts/> */}
-      //     {/* <PolizaContract /> */}
-      //     <Fd/>
-      //   </div>
-      // </div>
 
 
     
